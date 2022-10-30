532fb7308a8689a95703f0fe5ecfb85e0de2209a
qXoap <tbralex7@gmail.com>
2022-10-29 22:07:57 -0500

    Add setHome Command and more

--- src/qXoap/EasyHomes/commands/AddHome.php
+++ src/qXoap/EasyHomes/commands/AddHome.php
@@ -4,6 +4,10 @@ namespace qXoap\EasyHomes\commands;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+use pocketmine\utils\TextFormat;
+use qXoap\EasyHomes\Loader;
+use qXoap\EasyHomes\manager\HomeManager;
 
 class AddHome extends Command {
 
@@ -12,8 +16,35 @@ class AddHome extends Command {
         parent::__construct("addhome", "Add New Home (qXoap)", null, ["sethome"]);
     }
 
-    public function execute(CommandSender $sender, string $commandLabel, array $args)
+    public function execute(CommandSender $player, string $commandLabel, array $args)
     {
+        if(!$player instanceof Player)return;
+
+        if(!isset($args[0])){
+            $player->sendMessage(HomeManager::getPrefix()."Usage /addhome (name)");
+            return;
+        }
+
+        $name = implode(" ", $args);
+
+        if(HomeManager::getInstance()->isHomeExist($player, $name)){
+            $player->sendMessage(HomeManager::getPrefix().Loader::getInstance()->getHomeMesages("home-exist-message"));
+            return;
+        }
+
+        $maxhomes = Loader::getInstance()->getHomeConfig("max-homes");
+
+        if(count(HomeManager::getInstance()->getHomes($player)->getAll(true)) > $maxhomes){
+            $msg = Loader::getInstance()->getHomeMesages("homes-limit-message");
+            $msg = str_replace("{MAXHOMES}", $maxhomes, $msg);
+            $player->sendMessage(HomeManager::getPrefix().TextFormat::colorize($msg));
+            return;
+        }
+
+        HomeManager::getInstance()->setHome($player, $name, $player->getPosition());
+        $msg = Loader::getInstance()->getHomeMesages("created-home-message");
+        $msg = str_replace("{HOME}", $name, $msg);
+        $player->sendMessage(HomeManager::getPrefix().TextFormat::colorize($msg));
 
     }
 }
--- src/qXoap/EasyHomes/manager/HomeManager.php
+++ src/qXoap/EasyHomes/manager/HomeManager.php
@@ -5,10 +5,18 @@ namespace qXoap\EasyHomes\manager;
 use Forms\FormAPI\SimpleForm;
 use pocketmine\player\Player;
 use pocketmine\utils\Config;
+use pocketmine\utils\SingletonTrait;
+use pocketmine\utils\TextFormat;
 use pocketmine\world\Position;
 use qXoap\EasyHomes\Loader;
 
 class HomeManager {
+    use SingletonTrait;
+
+    public function __construct()
+    {
+        self::setInstance($this);
+    }
 
     public function getHomes(Player $player): Config
     {
@@ -28,7 +36,7 @@ class HomeManager {
 
     public static function getPrefix()
     {
-        return Loader::getInstance()->getHomeMesages("prefix");
+        return TextFormat::colorize(Loader::getInstance()->getHomeMesages("prefix"));
     }
 
     public function setHome(Player $player, string $name, Position $position)
@@ -56,6 +64,7 @@ class HomeManager {
             if($data === null){
                 return;
             }
+
             if(count($this->getHomes($player)->getAll(true)) < 0){
                 return;
             }
@@ -65,7 +74,7 @@ class HomeManager {
             }
 
             if(!$this->isHomeExist($player, $data)){
-                $player->sendMessage(self::getPrefix().Loader::getInstance()->getHomeMesages("no-exist-home"));
+                $player->sendMessage(self::getPrefix().TextFormat::colorize(Loader::getInstance()->getHomeMesages("no-exist-home")));
                 return;
             }
 
@@ -77,7 +86,7 @@ class HomeManager {
             $player->teleport(new Position($x, $y, $z, $world));
             $message = Loader::getInstance()->getHomeMesages("teleport-home-message");
             $message = str_replace("{HOME}", $name, $message);
-            $player->sendMessage(self::getPrefix().$message);
+            $player->sendMessage(self::getPrefix().TextFormat::colorize($message));
 
         });
         $menu->setTitle("§8".$player->getName()."§5 Homes");
@@ -91,6 +100,46 @@ class HomeManager {
         }
         foreach ($this->getHomes($player)->getAll(true) as $home) {
             $menu->addButton($home."\nTap To Teleport",0,"textures/ui/icon_map", $home);
+            return;
+        }
+        $player->sendForm($menu);
+    }
+
+    public function getRemoveHomes(Player $player): void
+    {
+        $menu = new SimpleForm(function (Player $player, int $data = null){
+            if($data === null){
+                return;
+            }
+
+            if(is_null($this->getHomes($player)->getAll(true))){
+                return;
+            }
+
+            if(count($this->getHomes($player)->getAll(true)) < 0){
+                return;
+            }
+
+            if(!$this->isHomeExist($player, $data)){
+                $player->sendMessage(self::getPrefix().TextFormat::colorize(Loader::getInstance()->getHomeMesages("no-exist-home")));
+                return;
+            }
+
+            $this->removeHome($player, $data);
+            $player->sendMessage(self::getPrefix().TextFormat::colorize(Loader::getInstance()->getHomeMesages("remove-home-message")));
+
+        });
+        $menu->setTitle("§8".$player->getName()."§c Homes");
+        if(is_null($this->getHomes($player)->getAll(true))){
+            $menu->addButton("§l§4EXIT",0,"textures/ui/redX1");
+            return;
+        }
+        if(count($this->getHomes($player)->getAll(true)) < 0){
+            $menu->addButton("§l§4EXIT",0,"textures/ui/redX1");
+            return;
+        }
+        foreach ($this->getHomes($player)->getAll(true) as $home) {
+            $menu->addButton($home."\nTap To Remove Home",0,"textures/ui/icon_none", $home);
             return;
         }
         $player->sendForm($menu);
